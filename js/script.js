6317f3abd8c7c2b8e8a2c90329eaa09cd137dfa6
jimmy-oss <jimmynjonge51@gmail.com>
2022-08-21 11:55:16 +0300

    filtering the class icons

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 var icons = document.querySelectorAll('.section-1-icons i')
+let i = 1
 
 setInterval(()=>{
+  i++
 
   var icon = document.querySelector('.section-1-icons .change')
 
   icon.classList.remove('change')
-  icon.nextElementSibling.classList.add('change')
+  if(i > icons.length){
+    icons[0].classList.add('change')
+    i = 1
+  }else{
+    icon.nextElementSibling.classList.add('change')
+  }
+   
 
 },1000)
